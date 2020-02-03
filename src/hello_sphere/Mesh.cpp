aac9db89ee81df478ea1424e70ed8840fcb6cc55
Mathis MARGOT <mathis.margot98@gmail.com>
2020-02-03 15:32:09 +0100

    add UV sphere generation

--- src/hello_sphere/Mesh.cpp
+++ src/hello_sphere/Mesh.cpp
@@ -35,6 +35,9 @@ void Mesh::setupMesh() {
     /// in vertex normal
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+    /// in vertex texture coords
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
     /// release
     glBindVertexArray(0);
@@ -46,4 +49,40 @@ void Mesh::draw(const Shader &shader) const {
     glBindVertexArray(m_VAO);
     glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
+}
+
+/// Primitives
+Mesh createUVSphere(int meridians, int parallels) {
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+    Vertex vertex;
+
+    float z, xy;
+
+    float parallelStep = 2.f * glm::pi<float>() / parallels;
+    float meridianStep = glm::pi<float>() / meridians;
+    float parallelAngle, meridianAngle;
+
+    for (int i = 0 ; i <= parallels ; ++i) {
+        parallelAngle = glm::pi<float>() / 2.f - i * parallelStep;
+        xy = glm::cos(parallelAngle);
+        z = glm::sin(parallelAngle);
+
+        for (int j = 0 ; j <= meridians ; ++j) {
+            meridianAngle = j * meridianStep;
+
+            vertex.Position = vertex.Normal = glm::vec3(
+                    xy * glm::cos(meridianAngle),
+                    xy * glm::sin(meridianAngle),
+                    z
+                    );
+
+            vertex.TexCoords = glm::vec2(
+                    (float)j / meridians,
+                    (float)i / parallels
+                    );
+
+            vertices.push_back(vertex);
+        }
+    }
 }
--- src/hello_sphere/Mesh.hpp
+++ src/hello_sphere/Mesh.hpp
@@ -14,7 +14,7 @@ struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
     //glm::vec3 Tangent;
-    //glm::vec2 TexCoords;
+    glm::vec2 TexCoords;
 };
 
 class Mesh {
