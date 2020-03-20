b76ac2aac3eb34a1977fccd53aef5b650c2e9b15
DaftMat <mathis.margot98@gmail.com>
2020-03-20 18:01:04 +0100

    add 3rdPartyLibs dir and move deps to it
    add stb_image and lodepng

---
+++ src/Engine/Rendering/Raytracer/Raytracer.cpp
@@ -0,0 +1,5 @@
+/**
+ * Created by mathis on 20/03/2020.
+ */
+
+#include "Raytracer.hpp"
---
+++ src/Engine/Rendering/Raytracer/Raytracer.hpp
@@ -0,0 +1,14 @@
+/**
+ * Created by mathis on 20/03/2020.
+ */
+
+#ifndef DAFT_ENGINE_RAYTRACER_HPP
+#define DAFT_ENGINE_RAYTRACER_HPP
+
+
+class Raytracer {
+
+};
+
+
+#endif //DAFT_ENGINE_RAYTRACER_HPP
